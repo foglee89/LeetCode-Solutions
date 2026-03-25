88ef8c6010c03e239c49fa9c18fbf9f3651341ee
Erik Fogle <efogle89@gmail.com>
2026-03-25 12:54:03 -0600

    0169-MAJORITY-ELEMENT Stats: Time: 3 ms (85.93%), Space: 21 MB (95.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -6,9 +6,6 @@ class Solution:
         for num in nums:
             if count == 0:
                 candidate = num
-            if num != candidate:
-                count -= 1
-            else:
-                count += 1
+            count += (1 if num == candidate else -1)
 
         return candidate
