5ad78a52d230c0ece3c9b837525aa7738d30e684
Erik Fogle <efogle89@gmail.com>
2026-03-28 12:54:42 -0600

    0219-CONTAINS-DUPLICATE-II Stats: Time: 44 ms (16.25%), Space: 32.5 MB (73.54%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -4,14 +4,13 @@ class Solution:
         window = set()
 
         for R in range(len(nums)):
-
             if R-L > k:
                 window.remove(nums[L])
                 L += 1
-            
+
             if nums[R] in window:
                 return True
 
             window.add(nums[R])
-        
+
         return False
