52eec64591d3e4268660a012dcca044c3c370cd5
Erik Fogle <efogle89@gmail.com>
2026-03-28 12:38:54 -0600

    0080-REMOVE-DUPLICATES-FROM-SORTED-ARRAY-II Stats: Time: 86 ms (64.59%), Space: 22 MB (70.07%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -3,11 +3,13 @@ class Solution:
         ci = 2
 
         for i in range(2, len(nums)):
+
             if nums[i] != nums[ci-1]:
                 nums[ci] = nums[i]
                 ci += 1
+
             elif nums[i] != nums[ci-2]:
                 nums[ci] = nums[i]
                 ci += 1
-            
+
         return ci
