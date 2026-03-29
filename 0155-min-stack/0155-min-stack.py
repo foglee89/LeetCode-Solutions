4a74a596f62e82cbdc53a098206f14be055495a8
Erik Fogle <efogle89@gmail.com>
2026-03-29 14:09:21 -0600

    0155-MIN-STACK Stats: Time: 11 ms (22.34%), Space: 22.5 MB (51.74%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -6,11 +6,12 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        val = min(val, self.min_stack[-1] if self.min_stack else val)
+        val = min(val, self.min_stack[-1]) if self.min_stack  else val
         self.min_stack.append(val)
+
     def pop(self) -> None:
-        self.stack.pop()
         self.min_stack.pop()
+        return self.stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
