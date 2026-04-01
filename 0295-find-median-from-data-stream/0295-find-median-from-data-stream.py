6806d694555c3499f22d3b3799c7532999eceda4
Erik Fogle <efogle89@gmail.com>
2026-04-01 14:59:29 -0600

    0295-FIND-MEDIAN-FROM-DATA-STREAM Stats: Time: 195 ms (45.88%), Space: 41.7 MB (76.78%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -5,14 +5,31 @@ class MedianFinder:
         self.large = []
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.large, heapq.heappushpop_max(self.small, num))
-        if len(self.large) > len(self.small):
+        # elegant approach, req more comments
+        # # push every new num to small max heap and pop largest small and push to large min heap
+        # heapq.heappush(self.large, heapq.heappushpop_max(self.small, num))
+        # # auto rebalance all odd qty sets to small max heap for faster median lookup
+        # if len(self.large) > len(self.small):
+        #     heapq.heappush_max(self.small, heapq.heappop(self.large))
+
+        # long approach, more self-explanatory and readily-adaptable
+        heapq.heappush_max(self.small, num)
+
+        if (self.small and self.large) and (self.small[0] > self.large[0]):
+            heapq.heappush(self.large, heapq.heappop_max(self.small))
+
+        if len(self.small) > len(self.large) + 1:
+            heapq.heappush(self.large, heapq.heappop_max(self.small))
+
+        if len(self.small) + 1 < len(self.large):
             heapq.heappush_max(self.small, heapq.heappop(self.large))
-        
 
     def findMedian(self) -> float:
         if len(self.small) > len(self.large):
             return self.small[0]
+        # req elif for long approach
+        elif len(self.small) < len(self.large):
+            return self.large[0]
         else:
             return (self.small[0] + self.large[0]) / 2
 
