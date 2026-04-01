976f0c51cc302a2cd94015658feb6b6d203876df
Erik Fogle <efogle89@gmail.com>
2026-04-01 13:59:15 -0600

    0383-RANSOM-NOTE Stats: Time: 15 ms (76.46%), Space: 19.8 MB (22.32%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,15 +1,10 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-
-        if len(ransomNote) > len(magazine):
-            return False
-
-        counts_magazine = collections.Counter(magazine)
-
-        for ch in ransomNote:
-            if counts_magazine[ch] <= 0:
-                return False
-            
-            counts_magazine[ch] -= 1
+        magLetters = collections.Counter(magazine)
         
+        for c in ransomNote:
+            if magLetters[c] <= 0:
+                return False
+            else:
+                magLetters[c] -= 1
         return True
