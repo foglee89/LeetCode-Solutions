91a0e919e19d62a3deb5133713860acb35b020f3
Erik Fogle <efogle89@gmail.com>
2026-04-01 14:34:31 -0600

    0078-SUBSETS Stats: Time: 0 ms (100%), Space: 19.4 MB (79.35%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,7 +1,24 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
+        # res = [[]]
 
-        for num in nums:
-            res += [subset + [num] for subset in res]
-        return res
+        # for num in nums:
+        #     res += [subset + [num] for subset in res]
+        # return res
+
+        curSet, subsets = [], []
+        self.helperUnique(0, nums, curSet, subsets)
+        return subsets
+
+    def helperUnique(self, i, nums, curSet, subsets):
+        if i >= len(nums):
+            subsets.append(curSet.copy())
+            return
+
+        # inc nums[i]
+        curSet.append(nums[i])
+        self.helperUnique(i+1, nums, curSet, subsets)
+        curSet.pop()
+
+        # not inc nums[i]
+        self.helperUnique(i+1, nums, curSet, subsets)
