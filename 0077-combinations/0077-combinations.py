e2c9c3972e3754096851fd5e6327ccf49ba9dc7b
Erik Fogle <efogle89@gmail.com>
2026-04-02 14:31:02 -0600

    0077-COMBINATIONS Stats: Time: 107 ms (67.1%), Space: 61.5 MB (18.75%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,39 +1,28 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         combinations = []
-        self.helper(1, [], combinations, n, k)
+
+        def helper(i, curComb):
+            if len(curComb) == k:
+                combinations.append(curComb.copy())
+                return
+            if i > n:
+                return
+            # sub-optimal backtracking
+            # # to inc i
+            # curComb.append(i)
+            # helper(i+1, curComb)
+            # curComb.pop()
+
+            # # not inc i
+            # helper(i+1, curComb)
+
+            # optimal backtracking
+
+            for j in range(i, n+1):
+                curComb.append(j)
+                helper(j+1, curComb)
+                curComb.pop()
+
+        helper(1, [])
         return combinations
-
-    def helper(self, i, curComb, combinations, n, k):
-        if len(curComb) == k:
-            combinations.append(curComb.copy())
-            return
-        if i > n:
-            return
-
-        # to inc i
-        curComb.append(i)
-        self.helper(i+1, curComb, combinations, n, k)
-        curComb.pop()
-
-        # not inc i
-        self.helper(i+1, curComb, combinations, n, k)
-
-        # def helper(i, curComb):
-        #     if len(curComb) == k:
-        #         combinations.append(curComb)
-        #         return
-        #     if i > n:
-        #         return
-
-        #     # to inc i
-        #     curComb.append(i)
-        #     helper(i+1, curComb)
-        #     curComb.pop()
-
-        #     # not inc i
-        #     helper(i+1, curComb)
-
-        # helper(1, [])
-        # return combinations
-
