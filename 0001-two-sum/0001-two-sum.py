7cf336ceddad19e5ba4b55c11ea8a54b6e3bbcd9
Erik Fogle <efogle89@gmail.com>
2026-04-04 12:14:05 -0600

    0001-TWO-SUM Stats: Time: 0 ms (100%), Space: 20.4 MB (63.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,8 +4,10 @@ class Solution:
         available = {}
 
         for i,n in enumerate(nums):
-            if n not in available:
-                available[n] = i
             
             if target-n in available and available[target-n] != i:
                 return [available[target-n], i]
+            
+            available[n] = i
+        
+        return []
