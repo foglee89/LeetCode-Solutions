962e22832ef72fdc8d93abaad72b99f5def90ebc
Erik Fogle <efogle89@gmail.com>
2026-04-04 12:11:43 -0600

    0001-TWO-SUM Stats: Time: 2 ms (55.66%), Space: 20.6 MB (30.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        available = collections.defaultdict(list)
-        for i, n in enumerate(nums):
-            available[n].append(i)
+        available = {}
 
         for i,n in enumerate(nums):
+            if n not in available:
+                available[n] = i
+
             t = target-n
             
-            if t in available:
-                ti = available[t][0]
-                if len(available[t]) == 2:
-                    return available[t]
-                elif ti == i:
-                    continue
-                else:
-                    return [i, ti]
+            if t in available and available[t] != i:
+                return [available[t], i]
+                # ti = available[t][0]
+                # if len(available[t]) == 2:
+                #     return available[t]
+                # elif ti == i:
+                #     continue
+                # else:
+                #     return [i, ti]
