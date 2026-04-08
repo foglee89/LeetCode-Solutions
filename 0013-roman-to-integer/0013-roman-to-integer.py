48194c20758cebf8134a7898ae43a4691807f351
Erik Fogle <efogle89@gmail.com>
2026-04-07 21:58:45 -0600

    0013-ROMAN-TO-INTEGER Stats: Time: 0 ms (100%), Space: 19.4 MB (10.27%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -3,13 +3,10 @@ class Solution:
         romanDict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         tot = 0
         l = len(s)-1
-        for i, r in enumerate(s):
+        for i in range(l, -1, -1):
+            r = s[i]
             
-            if r == 'I' and i < l and (s[i+1] == 'V' or s[i+1] == 'X'):
-                tot -= romanDict[r]
-            elif r == 'X' and i < l and (s[i+1] == 'L' or s[i+1] == 'C'):
-                tot -= romanDict[r]
-            elif r == 'C' and i < l and (s[i+1] == 'D' or s[i+1] == 'M'):
+            if i < l and romanDict[r] < romanDict[s[i+1]]:
                 tot -= romanDict[r]
             else:
                 tot += romanDict[r]
